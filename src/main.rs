0d8f5cd37cd9c629318a96cf92e1b79506ab1e3e
wutterfly <105152123+wutterfly@users.noreply.github.com>
2023-12-13 11:48:46 +0100

    init example

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use futures_util::{SinkExt, StreamExt};
+use std::{net::SocketAddr, time::Duration};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{
+    accept_async,
+    tungstenite::{Error, Message, Result},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?;
+
+    rt.block_on(start_server())?;
+    Ok(())
+}
+
+async fn start_server() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:8080";
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await?;
+    let listener = try_socket;
+
+    println!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let peer = stream
+            .peer_addr()
+            .expect("connected streams should have a peer address");
+        tokio::spawn(accept_connection(peer, stream));
+    }
+
+    Ok(())
+}
+
+async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
+    if let Err(e) = handle_connection(peer, stream).await {
+        match e {
+            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+            err => println!("Error processing connection: {}", err),
+        }
+    }
+}
+
+async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
+    let ws_stream = accept_async(stream).await?;
+    println!("New WebSocket connection: {}", peer);
+    let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+    let mut interval = tokio::time::interval(Duration::from_millis(2000));
+
+    // Echo incoming WebSocket messages and send a message periodically every second.
+    loop {
+        tokio::select! {
+            msg = ws_receiver.next() => {
+                match msg {
+                    Some(msg) => {
+                        let msg = msg?;
+                        if msg.is_text() ||msg.is_binary() {
+                            ws_sender.send(msg).await?;
+                        } else if msg.is_close() {
+                            println!("Connection closed: {}", peer);
+                            break;
+                        }
+                    }
+                    None => break,
+                }
+            }
+            _ = interval.tick() => {
+                ws_sender.send(Message::Text("tick".to_owned())).await?;
+            }
+        }
+    }
+
+    Ok(())
+}
